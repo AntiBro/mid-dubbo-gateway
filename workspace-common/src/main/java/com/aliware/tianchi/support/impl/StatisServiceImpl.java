6923bf60acff1fbdf7e70e9b4a612fef302e63a1
AntiBro <291015924@qq.com>
2019-07-13 15:51:09 +0800

    参数调整

--- workspace-common/src/main/java/com/aliware/tianchi/support/impl/StatisServiceImpl.java
+++ workspace-common/src/main/java/com/aliware/tianchi/support/impl/StatisServiceImpl.java
@@ -20,7 +20,7 @@ public class StatisServiceImpl implements StatisService {
 
     private static StatisService INSTANCE = new StatisServiceImpl();
 
-    static final int size = 50;
+    static final int size = 30;
 
     static final double size_d = size;
 
